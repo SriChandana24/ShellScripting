4524c38ed1c8761d308bbbdffb75832ac9ccc2d3
SriChandana24 <srichandana246@gmail.com>
2022-01-27 16:26:17 +0530

    Updated cart.sh with shellscripting code

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,25 +1,97 @@
-source components/common.sh
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
 
-echo "Installing NGINX"
-yum install nginx -y &>>$LOG_FILE
+## BUG about reaching one endpoint , To fix this we are using this command
+rm -f /etc/yum.repos.d/endpoint.repo
 
-echo "Downloading frontend code"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit 2
+  fi
+}
 
-echo "Clean Old Content"
-rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
+APP_USER_SETUP_WITH_APP() {
+  echo "Create App User"
+  id roboshop  &>>$LOG_FILE
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>$LOG_FILE
+  fi
+  STAT $?
 
-echo "Extract Frontend Content"
-cd /tmp
-unzip -o frontend.zip &>>$LOG_FILE
+  echo "Download ${COMPONENT} Code"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG_FILE
+  STAT $?
 
-echo "Copy Extracted Content to Nginx Path"
-cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
+  echo "Extract ${COMPONENT} Code"
+  cd /tmp/
+  unzip -o ${COMPONENT}.zip &>>$LOG_FILE
+  STAT $?
 
+  echo "Clean Old ${COMPONENT} Content"
+  rm -rf /home/roboshop/${COMPONENT}
+  STAT $?
 
-echo "Copy Nginx RoboShop Config"
-cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+  echo "Copy ${COMPONENT} Content"
+  cp -r ${COMPONENT}-main /home/roboshop/${COMPONENT} &>>$LOG_FILE
+  STAT $?
+}
 
-echo "Start Nginx Service"
-systemctl enable nginx &>>$LOG_FILE
-systemctl start nginx  &>>$LOG_FILE
+SYSTEMD_SETUP() {
+  chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
+
+  echo "Update ${COMPONENT} SystemD file"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
+  STAT $?
+
+  echo "Setup ${COMPONENT} SystemD file"
+  mv /home/roboshop/${COMPONENT}/systemd.service  /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
+  STAT $?
+
+  echo "Start ${COMPONENT} Service"
+  systemctl daemon-relaod  &>>$LOG_FILE
+  systemctl enable ${COMPONENT} &>>$LOG_FILE
+  systemctl restart ${COMPONENT} &>>$LOG_FILE
+  STAT $?
+
+}
+
+NODEJS() {
+  COMPONENT=$1
+  echo "Setup NodeJS repo"
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>$LOG_FILE
+  STAT $?
+
+  echo "Install NodeJS"
+  yum install nodejs gcc-c++ -y &>>$LOG_FILE
+  STAT $?
+
+  APP_USER_SETUP_WITH_APP
+
+  echo "Install NodeJS Dependencies"
+  cd /home/roboshop/${COMPONENT}
+  npm install &>>$LOG_FILE
+  STAT $?
+
+  SYSTEMD_SETUP
+}
+
+JAVA() {
+  COMPONENT=$1
+
+  echo "Install Maven"
+  yum install maven -y &>>$LOG_FILE
+  STAT $?
+
+  APP_USER_SETUP_WITH_APP
+
+  echo "Compile ${COMPONENT} Code"
+  cd /home/roboshop/${COMPONENT}
+  mvn clean package &>>$LOG_FILE
+  mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
+  STAT $?
+
+  SYSTEMD_SETUP
+}
